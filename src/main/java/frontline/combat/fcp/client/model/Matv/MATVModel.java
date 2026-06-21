b0de5a89168372a65b6dfb1451071110b58671f0
Connor <connordunn355@gmail.com>
2026-06-21 14:51:38 +0100

    Fixed Matv Again

--- src/main/java/frontline/combat/fcp/client/model/Matv/MATVModel.java
+++ src/main/java/frontline/combat/fcp/client/model/Matv/MATVModel.java
@@ -2,12 +2,19 @@ package frontline.combat.fcp.client.model.Matv;
 
 import com.atsuishio.superbwarfare.client.model.entity.VehicleModel;
 import frontline.combat.fcp.FCP;
+import frontline.combat.fcp.client.model.FCPVehicleModel;
+import frontline.combat.fcp.client.model.Util.CannonRecoilTransforms;
+import frontline.combat.fcp.client.model.Util.ModelBoneTransforms;
+import frontline.combat.fcp.entity.vehicle.Lav.Lav25Entity;
 import frontline.combat.fcp.entity.vehicle.Matv.MATVEntity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.Nullable;
 
-public class MATVModel extends VehicleModel<MATVEntity> {
+public class MATVModel extends FCPVehicleModel<MATVEntity> {
+
+    private static final String CANNON_WEAPON = "PassengerMachineGun";
+
     @Override
     public ResourceLocation getModelResource(MATVEntity animatable) {
         return new ResourceLocation(FCP.MODID, "geo/matv.geo.json");
@@ -22,6 +29,8 @@ public class MATVModel extends VehicleModel<MATVEntity> {
     @Override
     public @Nullable VehicleModel.TransformContext<MATVEntity> collectTransform(String boneName) {
         return switch (boneName) {
+
+            case "BarrelOccilator" -> barrelRecoil(0);
 
             case "WheelL0Turn", "WheelR0Turn" -> (bone, vehicle, state) -> {
                 float wheelRot = Mth.lerp(state.getPartialTick(), vehicle.getPrevWheelRotation(), vehicle.getWheelRotation());
@@ -38,6 +47,19 @@ public class MATVModel extends VehicleModel<MATVEntity> {
                 bone.setRotX((float) Math.toRadians(-wheelRot));
             };
             default -> super.collectTransform(boneName);
+        };
+    }
+
+    private VehicleModel.TransformContext<MATVEntity> barrelRecoil(int barrelIndex) {
+        return (bone, vehicle, state) -> {
+            ModelBoneTransforms.clearRecoilOffsets(bone);
+            if (vehicle.getCannonRecoilTime() <= 0) {
+                return;
+            }
+            if (!CANNON_WEAPON.equals(vehicle.getGunName(1))) {
+                return;
+            }
+            CannonRecoilTransforms.apply(bone, vehicle, CannonRecoilTransforms.Profile.SIDETOSIDE);
         };
     }
 }
