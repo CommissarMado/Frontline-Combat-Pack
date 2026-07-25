a5e008e772b2a3364aa87560bce9b1286762e22e
Connor <connordunn355@gmail.com>
2026-07-25 21:36:41 +0100

    File Restructuring

---
+++ src/main/java/frontline/combat/fcp/client/renderer/Bmp/Bmp1/BMP1URenderer.java
@@ -0,0 +1,17 @@
+package frontline.combat.fcp.client.renderer.Bmp.Bmp1;
+
+import com.atsuishio.superbwarfare.client.renderer.entity.VehicleRenderer;
+import frontline.combat.fcp.client.model.Bmp.Bmp1.BMP1UModel;
+import frontline.combat.fcp.entity.vehicle.Bmp.Bmp1.BMP1UEntity;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+public class BMP1URenderer extends VehicleRenderer<BMP1UEntity> {
+
+    public BMP1URenderer(EntityRendererProvider.Context renderManager) { super(renderManager, new BMP1UModel());}
+
+    @Override
+    public ResourceLocation getTextureLocation(BMP1UEntity entity) {
+        return entity.getCurrentTexture();
+    }
+}
