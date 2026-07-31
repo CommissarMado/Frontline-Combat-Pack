6ee9003da7eccd79fb916dfda5d9d3f4b5a269e4
nidomas <nidomas123@gmail.com>
2026-07-31 11:35:58 +0300

    Ural cleanup

--- src/main/java/frontline/combat/fcp/client/model/Kamaz/KamazLongModel.java
+++ src/main/java/frontline/combat/fcp/client/model/Kamaz/KamazLongModel.java
@@ -12,9 +12,9 @@ public class KamazLongModel extends FCPVehicleModel<KamazLongEntity> {
     @Override public ResourceLocation getModelResource(KamazLongEntity a) {return new ResourceLocation(FCP.MODID, "geo/kamaz_long.geo.json");}
     @Override public boolean hideForTurretControllerWhileZooming() {return false;}
     @Override public @Nullable VehicleModel.TransformContext<KamazLongEntity> collectTransform(String boneName) {
-        VehicleModel.TransformContext<KamazLongEntity> steer = WheelRotationTransforms.matchAnyTurn(boneName, 0.6, 30f, "whell2", "whell6");
+        VehicleModel.TransformContext<KamazLongEntity> steer = WheelRotationTransforms.matchAnyTurn(boneName, 0.6, 30f, "whell2", "whell6", "whell7", "whell8");
         if (steer != null) return steer;
-        VehicleModel.TransformContext<KamazLongEntity> wheels = WheelRotationTransforms.matchAny(boneName, 0.6, "whell7", "whell8", "whell3", "whell9", "whell4", "whell5");
+        VehicleModel.TransformContext<KamazLongEntity> wheels = WheelRotationTransforms.matchAny(boneName, 0.6, "whell3", "whell9", "whell4", "whell5");
         if (wheels != null) return wheels;
         return super.collectTransform(boneName);
     }
